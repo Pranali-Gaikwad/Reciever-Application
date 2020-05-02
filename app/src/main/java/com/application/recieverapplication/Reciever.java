3632f66b0ed1846c9dbf629c245a6384f3b0e77b
GaikwaPr <gaikwadpranali1995@gmail.com>
2020-05-02 20:41:51 +0530

    hiiii

---
+++ app/src/main/java/com/application/recieverapplication/Reciever.java
@@ -0,0 +1,15 @@
+package com.application.recieverapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class Reciever extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("SMS","onRecieve method");
+        Toast.makeText(context,"Broadcast Receiver triggered..!",Toast.LENGTH_SHORT).show();
+    }
+}
